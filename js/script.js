81efe5c7cd0961ba500d33c9c0b3e1d284a5b7e0
Munz1993 <112294417+Munz1993@users.noreply.github.com>
2022-11-17 15:22:37 -0600

    updated JS
    simplified the map js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
- function initMap(){
-  var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
+function initMap() {
+  // The location of MTCC
+  var mtcc = { lat: 41.835211513686694,lng: -87.62580689029369 }; 
+  var mapOptions = {
+    zoom: 19,
+    center: mtcc,
+	 mapTypeId: google.maps.MapTypeId.SATELLITE,
+	 mapTypeControlOptions: {
+		  position: google.maps.ControlPosition.TOP_LEFT
+		}
+  };
+  // The map, centered at MTCC
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions); 
+  
+  // The marker, positioned at MTCC
+  var marker = new google.maps.Marker({
+    position: mtcc,
+    map: map,
+  });
 }
-
-google.maps.event.addDomListener(window, 'load', initMap);
+google.maps.event.addDomListener(window, 'load', init);
